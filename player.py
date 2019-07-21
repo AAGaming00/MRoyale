258932e65d647bfb00fb14d71330770a8d59113a
Igor Pissolati <igo08an@hotmail.com>
2019-07-21 16:29:22 -0300

    Add a cute prefix for DEVs

--- player.py
+++ player.py
@@ -25,6 +25,21 @@ class Player(object):
             self.name = self.server.defaultName if self.client.username != "" else ("【G】"+self.server.defaultName)
         elif len(self.client.username) == 0:
             self.name = "【G】" + self.name
+        if self.username.lower() in ["taliondiscord",
+                                    "damonj17",
+                                    "ddmil@marioroyale:~$",
+                                    "pixelcraftian",
+                                    "igor",
+                                    "minus",
+                                    "cyuubi",
+                                    "gyorokpeter",
+                                    "zizzydizzymc",
+                                    "nuts & milk",
+                                    "jupitersky",
+                                    "nethowarrior",
+                                    "real novex",
+                                    "nightyoshi370"]:
+            self.name = "【𝐃𝐄𝐕】" + self.name
         self.pendingWorld = None
         self.level = int()
         self.zone = int()
