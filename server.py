d118f0776f03fb89481bc4af77cd78d2020619c7
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-03-24 16:13:42 -0400

    Update server.py

--- server.py
+++ server.py
@@ -464,7 +464,7 @@ class MyServerFactory(WebSocketServerFactory):
         config = configparser.ConfigParser()
         config.read('server.cfg')
 
-        self.listenPort = int(os.environ.get("PORT", 5000))
+        self.listenPort = config.get('Server', 'Port').strip()
         self.mcode = config.get('Server', 'MCode').strip()
         self.statusPath = config.get('Server', 'StatusPath').strip()
         self.defaultName = config.get('Server', 'DefaultName').strip()
