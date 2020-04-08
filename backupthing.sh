507d9f284dd0bd50a6bbd7fffdf617c8b1b0878e
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-04-08 16:13:42 -0400

    fix?

--- backupthing.sh
+++ backupthing.sh
@@ -2,4 +2,9 @@
 
 $b2args
 b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"
-watch -n 30 'b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"'
+#watch -n 30 'b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"'
+while [ true ]
+do
+    b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"
+    sleep 30
+done
