a133f083fbd9facc2a76f8a332aa74e1f2588ae1
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-04-08 17:51:02 -0400

    backup delay more

--- backupthing.sh
+++ backupthing.sh
@@ -3,11 +3,11 @@ cd ~/dat
 $b2args
 b2 download-file-by-name  MRoyale-acc server.dat ~/dat/server.dat
 #watch -n 30 'b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"'
-sleep 5
+sleep 20
 while [ true ]
 do
     b2 sync --keepDays 1  ~/dat "b2://MRoyale-acc/"
-    sleep 10
+    sleep 20
     ls
     cat ./server.dat
 done
