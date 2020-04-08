4f7736d5fc6a58e0f02b9d02d5732f95540b63ad
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-04-08 17:28:28 -0400

    fdjyguh

--- backupthing.sh
+++ backupthing.sh
@@ -3,6 +3,7 @@ cd ~/dat
 $b2args
 b2 sync --replaceNewer --keepDays 1 ~/dat "b2://MRoyale-acc/"
 #watch -n 30 'b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"'
+sleep 5
 while [ true ]
 do
     b2 sync --keepDays 1  ~/dat "b2://MRoyale-acc/"
