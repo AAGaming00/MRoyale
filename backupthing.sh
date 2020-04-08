db642c3dbbb88e0b8d3a9d5f7f1596a27488c934
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-04-08 17:27:04 -0400

    frestdgfhjy

--- backupthing.sh
+++ backupthing.sh
@@ -1,11 +1,11 @@
 # /bin/bash
 cd ~/dat
 $b2args
-b2 sync --replaceNewer --keepDays 0 ~/dat "b2://MRoyale-acc/"
+b2 sync --replaceNewer --keepDays 1 ~/dat "b2://MRoyale-acc/"
 #watch -n 30 'b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"'
 while [ true ]
 do
-    b2 sync --keepDays 0  ~/dat "b2://MRoyale-acc/"
+    b2 sync --keepDays 1  ~/dat "b2://MRoyale-acc/"
     sleep 10
     ls
 done
