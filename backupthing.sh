816bdd3c8da3fd8ac58bfd5a0f07dacaabe9246d
AAGithub <42613600+ADoesGit@users.noreply.github.com>
2020-04-08 16:15:48 -0400

    qwertyuiop

--- backupthing.sh
+++ backupthing.sh
@@ -1,10 +1,11 @@
 # /bin/bash
-
+cd ~
 $b2args
 b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"
 #watch -n 30 'b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"'
 while [ true ]
 do
     b2 sync --keepDays 0 /app/server.dat "b2://MRoyale-acc/"
-    sleep 30
+    sleep 10
+    ls
 done
